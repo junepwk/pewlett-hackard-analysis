d711b64264a4de2879b965611ab46d8037693b8e
June Pui <junepwk@gmail.com>
2021-06-13 20:03:28 -0400

    Updated query for mentorship eligibility table

--- queries/employee_database_challenge.sql
+++ queries/employee_database_challenge.sql
@@ -37,9 +37,12 @@ SELECT DISTINCT ON(e.emp_no) e.emp_no,
 	de.from_date,
 	de.to_date,
 	ti.title
---INTO mentorship_eligibilty
+INTO mentorship_eligibilty
 FROM employees as e
 	INNER JOIN dept_emp as de
 		ON (e.emp_no=de.emp_no)
 	INNER JOIN titles as ti
 		ON (de.emp_no=ti.emp_no)
+WHERE(e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
